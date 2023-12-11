4b08543adbe74c9afcd1da9d04f798dca869c95a
van-dt <dangvan31102001@gmail.com>
2023-12-11 22:50:29 +0700

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,25 @@
-import { GetServerSideProps } from 'next';
-import dbConnect from '../lib/dbConnect';
+import { GetServerSideProps } from "next";
+import dbConnect from "../lib/dbConnect";
+import Home from "../components/Home";
 
 type Props = {
   isConnected: boolean;
-}
+};
 
 const Index = ({ isConnected }: Props) => {
-  return (
-    <>
-      <h1>Welcome to Next.js!</h1>
-      <p>Build something amazing!</p>
-      <p>
-        {isConnected
-          ? 'You are connected to MongoDB'
-          : 'You are NOT connected to MongoDB. Please check the .env file to make sure you entered your MONGODB_URL correctly and try again.'}
-      </p>
-    </>
-  )
-}
+  return <Home />;
+};
 
 /* Trying connect to mongodb */
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   try {
-    await dbConnect()
+    await dbConnect();
 
-    return { props: { isConnected: true } }
+    return { props: { isConnected: true } };
   } catch (err) {
-    console.log(err)
-    return { props: { isConnected: false } }
+    console.log(err);
+    return { props: { isConnected: false } };
   }
-}
+};
 
-export default Index
+export default Index;
