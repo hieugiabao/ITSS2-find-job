946cff4946593d893f2bfed08d783f8b389ee9e7
hieugiabao <hieugiabao12@gmail.com>
2023-12-31 18:54:11 +0700

    fix build

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -1,8 +1,7 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import * as crypto from "crypto";
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,85 +17,3 @@ export const firebaseApp =
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
-
-export async function getAuthenticatedAppForUser(
-  session: string | null | undefined = null
-) {
-  if (typeof window !== "undefined") {
-    // client
-    console.log("client: ", firebaseApp);
-    const auth = getAuth(firebaseApp);
-    return { app: firebaseApp, user: auth.currentUser?.toJSON() };
-  }
-
-  const { initializeApp: initializeAdminApp, getApps: getAdminApps } =
-    await import("firebase-admin/app");
-
-  const { getAuth: getAdminAuth } = await import("firebase-admin/auth");
-
-  const { credential } = await import("firebase-admin");
-
-  const ADMIN_APP_NAME = "ITSS2-find-job";
-  const adminApp =
-    getAdminApps().find((it) => it.name === ADMIN_APP_NAME) ??
-    initializeAdminApp(
-      {
-        credential: credential.applicationDefault(),
-      },
-      ADMIN_APP_NAME
-    );
-
-  const adminAuth = getAdminAuth(adminApp);
-  const noSessionReturn = { app: null, currentUser: null };
-
-  if (!session) {
-    // if no session cookie was passed, try to get from next/headers for app router
-    session = await getAppRouterSession();
-
-    if (!session) return noSessionReturn;
-  }
-
-  const decodedIdToken = await adminAuth.verifySessionCookie(session);
-
-  const app = initializeAuthenticatedApp(decodedIdToken.uid);
-  const auth = getAuth(app);
-
-  // handle revoked tokens
-  const isRevoked = !(await adminAuth
-    .verifySessionCookie(session, true)
-    .catch((e) => console.error(e.message)));
-  if (isRevoked) return noSessionReturn;
-
-  // authenticate with custom token
-  if (auth.currentUser?.uid !== decodedIdToken.uid) {
-    const customToken = await adminAuth
-      .createCustomToken(decodedIdToken.uid)
-      .catch((e) => console.error(e.message));
-
-    if (!customToken) return noSessionReturn;
-
-    await signInWithCustomToken(auth, customToken);
-  }
-  return { app, currentUser: auth.currentUser };
-}
-
-async function getAppRouterSession() {
-  // dynamically import to prevent import errors in pages router
-  const { cookies } = await import("next/headers");
-
-  try {
-    return cookies().get("__session")?.value;
-  } catch (error) {
-    // cookies() throws when called from pages router
-    return undefined;
-  }
-}
-
-function initializeAuthenticatedApp(uid: string) {
-  const random = crypto.randomBytes(36).toString("hex");
-  const appName = `authenticated-context:${uid}:${random}`;
-
-  const app = initializeApp(firebaseConfig, appName);
-
-  return app;
-}
